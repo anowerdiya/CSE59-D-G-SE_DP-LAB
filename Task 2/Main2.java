f488f6700387e2611ec094d2230fd61daa92533f
anowerdiya <anowerdiya@gmail.com>
2026-06-18 17:06:26 +0600

    Update Main2.java

--- Task 2/Main2.java
+++ Task 2/Main2.java
@@ -1,1 +1,18 @@
 
+
+class NumberChecker{
+    void check(int num){
+        System.out.println((num % 2 == 0) && (num > 10));
+    
+    }
+}
+
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        NumberChecker nc = new NumberChecker();
+        nc.check(14);
+    }
+    
+}
